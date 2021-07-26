196aa6459a11a0d264ac5330ef475c1119ff7732
inesa <inesa.yun@yahoo.com>
2021-07-26 12:52:41 -0400

    test cases all done

--- src/day1/ResettingPractice.java
+++ src/day1/ResettingPractice.java
@@ -4,4 +4,12 @@ public class ResettingPractice {
     //Test case part1 done
     //Test case part2 done
     //Test case part3 done
+    //Test case part4 done
+    //Test case part5 done
+    //Mixed|Soft reset remove the commit from hidtory but keep the local changes
+    // Did part 4 part 5 and I am done in one commit
+
+
+
+
 }
