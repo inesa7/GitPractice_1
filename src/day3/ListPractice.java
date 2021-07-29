181ff68af014ccffcbe167fef63679a46cf00f37
inesa <inesa.yun@yahoo.com>
2021-07-29 14:34:22 -0400

    list

---
+++ src/day3/ListPractice.java
@@ -0,0 +1,13 @@
+package day3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ListPractice {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+    }
+
+}
